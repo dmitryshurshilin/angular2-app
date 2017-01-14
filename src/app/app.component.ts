4d25b5ec79d83749e7545562c8e263c19ac474da
Dmitry Shurshilin <dima@sdweb.ru>
2017-01-14 22:55:50 +0400

    Apply styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,32 +13,32 @@ import { AppState } from './app.service';
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    './app.component.css'
+      './app.component.css'
   ],
   template: `
-    <nav>
-      <span>
-        <a [routerLink]=" ['./'] ">
-          Index
-        </a>
-      </span>
-      |
-      <span>
-        <a [routerLink]=" ['./books'] ">
-          Books
-        </a>
-      </span>
+    <nav class="navigation">
+        <ul class="navigation__items">
+            <li class="navigation__item"><a [routerLink]="['./']" class="navigation__link">Home</a></li>
+            <li class="navigation__item"><a [routerLink]="['./books']" class="navigation__link">Books</a></li>
+        </ul>
     </nav>
 
-    <main>
-      <router-outlet></router-outlet>
+    <header class="app-header">
+        <div class="app-header__content">
+            <div class="app-header__logo">
+                <a [routerLink]="['./']" class="app-header__link">Books <span>App</span></a>
+            </div>
+        </div>
+    </header>
+
+    <main class="app-main">
+        <div class="app-main__content">
+            <router-outlet></router-outlet>
+            <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+        </div>
     </main>
 
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-    <footer>
-      Angular App
-    </footer>
+    <footer class="app-footer">&copy; 2017 Angular App</footer>
   `
 })
 export class AppComponent {
