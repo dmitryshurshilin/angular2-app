e55dfe3711c7297559635775750fc4773543f1af
Dmitry Shurshilin <dima@sdweb.ru>
2017-01-20 23:49:11 +0400

    Redo links using routerLink

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { PipesModule } from '../pipes/pipes.module';
@@ -11,6 +12,7 @@ import { BookEditComponent } from './edit';
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule,
         FormsModule,
         PipesModule
     ],
