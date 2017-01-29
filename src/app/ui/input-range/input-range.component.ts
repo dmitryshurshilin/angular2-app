19c427c0310eba67589313509dcdae7a2a09af4a
Dmitry Shurshilin <dmitry.shurshilin@gmail.com>
2017-01-29 14:31:03 +0400

    WIP: Complete base functionality of input-range component

--- src/app/ui/input-range/input-range.component.ts
+++ src/app/ui/input-range/input-range.component.ts
@@ -1,24 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'input-range',  // <input-range></input-range>
     styleUrls: [ './input-range.component.css' ],
     templateUrl: './input-range.component.html',
-    { 
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => CounterInputComponent),
-      multi: true
-    }
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => InputRangeComponent),
+            multi: true
+        }
+    ]
 })
-export class InputRangeComponent implements ControlValueAccessor {
+export class InputRangeComponent {
 
-    @Input() min;
-    @Input() max;
-
+    @Input('inputValue') _inputValue: any;
+    propagateChange = (_: any) => {};
 
     constructor() {}
 
-}
+    writeValue(value: any) {
+        if (value !== undefined) {
+            this.inputValue = value;
+        }
+    }
 
-// https://blog.thoughtram.io/angular/2016/07/27/custom-form-controls-in-angular-2.html
+    registerOnChange(fn) {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched() {}
+
+    get inputValue() {
+        return this._inputValue;
+    }
+
+    set inputValue(value) {
+        this._inputValue = value;
+        this.propagateChange(value);
+    }
+
+    changeValue(e) {
+        this.inputValue = e.target.value;
+    }
+
+}
